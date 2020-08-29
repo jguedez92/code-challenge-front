d74508a9805217ec399000326b47466f5e9415e7
jguedez92 <jguedez92@gmail.com>
2020-08-29 20:03:54 +0200

    added a jumbotron on home page

--- src/views/pages/Home.jsx
+++ src/views/pages/Home.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>
-            This is the home page
+        <div className="col-12">
+            <div className="row d-flex justify-content-center py-5">
+                <div className="container">
+                    <div className="jumbotron bg-light p-5  my-5 animate__animated animate__bounceInUp">
+                        <h1 class="display-4">Bienvenidos!</h1>
+                        <hr class="my-4" />   
+                        <p class="lead">
+                            Esta es una web simple empleando Laravel en el Backend y React en el Frontend.
+                        </p>                     
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
