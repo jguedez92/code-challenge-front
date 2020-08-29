ab34a210e5c1eb59afdfd902d2d5d8b0dd878ac6
jguedez92 <jguedez92@gmail.com>
2020-08-29 19:20:57 +0200

    added fonst styles on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,22 +3,22 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark d-flex justify-content-center">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-center shadow">
             <div className="col-md-12 d-flex justify-content-center">
                 <div>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav mr-auto">
-                            <li class="nav-item active">
+                            <li class="nav-item ">
                                 <NavLink to='/' exact className="nav-link" >
                                     Home
                                 </NavLink>
                             </li>
-                            <li class="nav-item active">
+                            <li class="nav-item nav-title">
                                 <NavLink to='/users' exact className="nav-link" >
                                     Users
                                 </NavLink>
                             </li>
-                            <li class="nav-item active">
+                            <li class="nav-item nav-title">
                                 <NavLink to='/posts' exact className="nav-link" >
                                     Posts
                                 </NavLink>
